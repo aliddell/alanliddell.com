e3cb9a8f68dc08dd288fe32749662cb3d6133a6c
Alan Liddell <alan.c.liddell@gmail.com>
2021-05-07 19:37:50 -0400

    update post draft

--- content/posts/chebyshev-points.md
+++ content/posts/chebyshev-points.md
@@ -1,7 +1,17 @@
 ---
 title: "Chebyshev Points"
 date: 2021-04-27T20:51:32-04:00
+categories: "Approximation Theory"
 draft: true
 ---
 
+## Note
+
 This post follows along with *Approximation Theory and Approximation Practice*, Ch. 2: **Chebyshev points and interpolants**.
+I'm using [Pluto notebooks](https://juliahub.com/docs/Pluto/OJqMt/0.7.4/) for the exposition.
+
+My goal in writing this post is to learn about Chebyshev points and what makes them useful.
+
+## Chebyshev points
+
+![17 Chebyshev points on the interval $[-1, 1]$](/images/chebplot.svg)
--- notebooks/chebyshevpoints.jl
+++ notebooks/chebyshevpoints.jl
@@ -12,15 +12,19 @@ md"""Let's let $n = 16$.
 We'll generate the $n + 1$ Chebyshev points on the interval $[-1, 1]$ and plot them below.
 """
 
+# ╔═╡ c192cfda-f7d7-43c4-bd17-12e41703ff90
+points(Chebyshev([-1, 1]), 17)
+
 # ╔═╡ 8c03a235-48e0-495a-bf70-8f2ce8bfc16d
 begin
 	n = 16
 	pts = chebyshevpoints(n + 1)
-	plot(pts, cos.(pts); seriestype=:scatter)
+	plot(pts, cos.(pts); seriestype=:scatter, aspect_ratio=:equal, ylims=(-1, 1.1))
 	plot!(-1:0.01:1, cos.(-1:0.01:1))
 end
 
 # ╔═╡ Cell order:
 # ╠═6e221d46-a7c2-11eb-1862-a99748544e89
 # ╟─349d5de4-54b4-44e0-826a-a9de784be526
+# ╠═c192cfda-f7d7-43c4-bd17-12e41703ff90
 # ╠═8c03a235-48e0-495a-bf70-8f2ce8bfc16d
